64d8d6e19cc0a96f87171027a1093bff077ed554
Dweb Fan <dwebfan@gmail.com>
2024-04-20 22:49:30 -0700

    initial aws upload skeleton
    Signed-off-by: Dweb Fan <dwebfan@gmail.com>

--- cmd/lomob/upload.go
+++ cmd/lomob/upload.go
@@ -6,18 +6,11 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/lomorage/lomo-backup/clients"
 	"github.com/urfave/cli"
 )
 
-const (
-	maxPartSize = int64(5 * 1024 * 1024)
-	maxRetries  = 3
-)
+const maxPartSize = int64(5 * 1024 * 1024)
 
 func uploadISO(ctx *cli.Context) error {
 	accessKeyID := ctx.String("awsAccessKeyID")
@@ -29,17 +22,10 @@ func uploadISO(ctx *cli.Context) error {
 	fmt.Println(bucketRegion)
 	fmt.Println(bucketName)
 
-	creds := credentials.NewStaticCredentials(accessKeyID, secretAccessKey, "")
-	_, err := creds.Get()
+	cli, err := clients.NewUpload(accessKeyID, secretAccessKey, bucketRegion, clients.AWS)
 	if err != nil {
 		return err
 	}
-	cfg := aws.NewConfig().WithRegion(bucketRegion).WithCredentials(creds)
-	sess, err := session.NewSession()
-	if err != nil {
-		return nil
-	}
-	svc := s3.New(sess, cfg)
 
 	file, err := os.Open("test.jpg")
 	if err != nil {
@@ -57,13 +43,8 @@ func uploadISO(ctx *cli.Context) error {
 	file.Read(buffer)
 
 	path := "/media/" + file.Name()
-	input := &s3.CreateMultipartUploadInput{
-		Bucket:      aws.String(bucketName),
-		Key:         aws.String(path),
-		ContentType: aws.String(fileType),
-	}
 
-	resp, err := svc.CreateMultipartUpload(input)
+	request, err := cli.CreateMultipartUpload(bucketName, path, fileType)
 	if err != nil {
 		return err
 	}
@@ -71,7 +52,7 @@ func uploadISO(ctx *cli.Context) error {
 
 	var curr, partLength int64
 	var remaining = size
-	var completedParts []*s3.CompletedPart
+	var completedParts []clients.CompletePart
 	partNumber := 1
 	for curr = 0; remaining != 0; curr += partLength {
 		if remaining < maxPartSize {
@@ -79,81 +60,17 @@ func uploadISO(ctx *cli.Context) error {
 		} else {
 			partLength = maxPartSize
 		}
-		completedPart, err := uploadPart(svc, resp, buffer[curr:curr+partLength], partNumber)
+		etag, err := cli.Upload(int64(partNumber), int64(partLength), request, bytes.NewReader(buffer[curr:curr+partLength]), "")
 		if err != nil {
-			fmt.Println(err.Error())
-			err := abortMultipartUpload(svc, resp)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
 			return err
 		}
 		remaining -= partLength
 		partNumber++
-		completedParts = append(completedParts, completedPart)
+		completedParts = append(completedParts, clients.CompletePart{
+			PartNo: int64(partNumber),
+			Etag:   etag,
+		})
 	}
 
-	completeResponse, err := completeMultipartUpload(svc, resp, completedParts)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Successfully uploaded file: %s\n", completeResponse.String())
-	return nil
-}
-
-func completeMultipartUpload(svc *s3.S3, resp *s3.CreateMultipartUploadOutput, completedParts []*s3.CompletedPart) (*s3.CompleteMultipartUploadOutput, error) {
-	completeInput := &s3.CompleteMultipartUploadInput{
-		Bucket:   resp.Bucket,
-		Key:      resp.Key,
-		UploadId: resp.UploadId,
-		MultipartUpload: &s3.CompletedMultipartUpload{
-			Parts: completedParts,
-		},
-	}
-	return svc.CompleteMultipartUpload(completeInput)
-}
-
-func uploadPart(svc *s3.S3, resp *s3.CreateMultipartUploadOutput, fileBytes []byte, partNumber int) (*s3.CompletedPart, error) {
-	tryNum := 1
-	partInput := &s3.UploadPartInput{
-		Body:          bytes.NewReader(fileBytes),
-		Bucket:        resp.Bucket,
-		Key:           resp.Key,
-		PartNumber:    aws.Int64(int64(partNumber)),
-		UploadId:      resp.UploadId,
-		ContentLength: aws.Int64(int64(len(fileBytes))),
-	}
-
-	for tryNum <= maxRetries {
-		uploadResult, err := svc.UploadPart(partInput)
-		if err != nil {
-			if tryNum == maxRetries {
-				if aerr, ok := err.(awserr.Error); ok {
-					return nil, aerr
-				}
-				return nil, err
-			}
-			fmt.Printf("Retrying to upload part #%v\n", partNumber)
-			tryNum++
-		} else {
-			fmt.Printf("Uploaded part #%v\n", partNumber)
-			return &s3.CompletedPart{
-				ETag:       uploadResult.ETag,
-				PartNumber: aws.Int64(int64(partNumber)),
-			}, nil
-		}
-	}
-	return nil, nil
-}
-
-func abortMultipartUpload(svc *s3.S3, resp *s3.CreateMultipartUploadOutput) error {
-	fmt.Println("Aborting multipart upload for UploadId#" + *resp.UploadId)
-	abortInput := &s3.AbortMultipartUploadInput{
-		Bucket:   resp.Bucket,
-		Key:      resp.Key,
-		UploadId: resp.UploadId,
-	}
-	_, err := svc.AbortMultipartUpload(abortInput)
-	return err
+	return cli.CompleteMultipartUpload(request, completedParts, "")
 }
